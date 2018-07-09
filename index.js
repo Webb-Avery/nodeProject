767a4f41e692524b1f393fe3b96190bfec527719
Avery Webb <averymwebb@gmail.com>
2018-07-09 10:16:09 -0600

    adding stuff

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.set('port', (process.env.PORT || 5000))
   .get('/getDessert', function(req, res) {
         getDessert(req, res);
     })
-  .get('/login', function(req, res) {
+  .post('/login', function(req, res) {
       getUser(req, res);
     })
   .post('/addDessert', addDessert)
@@ -30,12 +30,10 @@ app.set('port', (process.env.PORT || 5000))
 
 
 function getDessert(req, response) {
-    var id = req.query.id;
-    getDessertFromDb(id, function(error, result) {
+    getDessertFromDb(function(error, result) {
         if (error || result == null || result.length != 1) {
            response.status(500).json({success: false, data:error}); 
         } else {
-            var person = result[0];
             response.status(200).json(result[0]);
         }
 
@@ -45,14 +43,12 @@ function getDessert(req, response) {
 
 
 
-function getDessertFromDb(id, callback){
+function getDessertFromDb( callback){
     console.log("Getting dessert from DB with id: " + id);
 
-    var sql = "SELECT id, name, description FROM dessert WHERE id = $1::int";
+    var sql = "SELECT id, name, description FROM dessert";
 
-    var params = [id];
-
-    pool.query(sql, params, function(err, result){
+    pool.query(sql, function(err, result){
         if (err){
             console.log("Error in query: ");
             console.log(err);
