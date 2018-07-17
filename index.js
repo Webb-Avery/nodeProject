36e0c56690b03c238ef41f2a6e26e48f94782ca3
Avery Webb <averymwebb@gmail.com>
2018-07-17 05:16:27 -0600

    attempting to fix stuff

--- index.js
+++ index.js
@@ -144,7 +144,7 @@ function getCommentFromDb(id, callback){
 }
 
 function getUser(req, response) {
-  
+  console.log("trying to get user");
     getUserFromDb(req, function(error, result) {
         if (error || result == null || result.length != 1) {
            response.status(500).json({success: false, message:'Username/Password incorrect'}); 
