a80ede9311956adc2444d5765352d34a166891f8
Avery Webb <averymwebb@gmail.com>
2018-07-18 15:20:01 -0600

    changing css

--- index.js
+++ index.js
@@ -51,12 +51,12 @@ app.set('port', (process.env.PORT || 5000))
       getComment(req, res);
 
     })  
-    .get('/getUser'), function(req, res) {
+    .get('/getUser', function(req, res) {
         username = req.session.user;
         res.status(500).json({user: username});
         
            
-    }
+    })
   .post('/addUser', addUser)
   .listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
